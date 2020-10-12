a7ae8c5a7685e9898438ba2be49d5ef5f79fcc71
tuanmeo <nqtuan87@gmail.com>
2020-10-12 16:04:03 +0700

    add cols_count function

--- PYTHON/3_python_data_science_toolbox(1)/twitter_analysis.py
+++ PYTHON/3_python_data_science_toolbox(1)/twitter_analysis.py
@@ -56,3 +56,47 @@ print(result1)
 for key, value in result1.items():
     print(key, ":", value)
 # %%
+# use lambda function and filter() to select retweets
+import pandas as pd
+tweets_df = pd.read_csv('tweets.csv')
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda x:x[0:2] == 'RT', tweets_df['text'])
+# Create list from filter object result: res_list
+res_list = list(result)
+# Print all tweet line by line
+for i in res_list:
+    print(i)
+# %%
+# Add try-except to function count_entries()
+import pandas as pd
+tweets_df = pd.read_csv('tweets.csv')
+
+def count_entries(df, col_name = 'lang'):
+    """Return a dictionary with counts of occurences as value for each key."""
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+    
+    # add try block
+    try:
+        # extract column from DataFrame: col
+        col = df[col_name]
+
+        # iterate over the column in dataframe
+        for i in col:
+            # If i is in cols_count, add 1
+            if i in cols_count:
+                cols_count[i] = cols_count[i] + 1
+            # else add i to cols_count, value = 1
+            else:
+                cols_count[i] = 1
+        # return the cols_count dictionary
+        return cols_count
+    except TypeError:
+        print('The DataFrame does not have a ' + cols_name+ ' column.')
+    
+# call count_entries(): result1
+result1 = count_entries(tweets_df,'lang')
+for key, value in result1.items():
+    print(key,":",value)
+
+# %%
