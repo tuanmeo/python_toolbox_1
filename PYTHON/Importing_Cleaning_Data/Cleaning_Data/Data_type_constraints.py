5d0b585172024b0505f515d6a364fa766dd3c4b7
tuanmeo <nqtuan87@gmail.com>
2020-12-03 21:42:40 +0700

    sorting files

--- PYTHON/Importing_Cleaning_Data/Cleaning_Data/Data_type_constraints.py
+++ PYTHON/Importing_Cleaning_Data/Cleaning_Data/Data_type_constraints.py
@@ -8,10 +8,35 @@
 
 #%%
 # Import packages
-
+import pandas as pd
 
 #%%
+# STRING TO INTEGERS
 # Import csv sales file
-sales = pd.read_csv('sales_example')
+sales = pd.read_csv('sales_example.csv')
 print(sales.head())
+
+# Print data type
+print(type(sales))
+
+# Print types of columns
+print(sales.dtypes)
+
+# Get DataFrame information
+print(sales.info())
+
+# Print sum of all Revenue column
+print(sales['Revenue'].sum()) --> # give text because $ sign
+
+# To sum revenue, need to remove $ sign, convert to int
+
+sales['Revenue'] = sales['Revenue'].str.strip('$')
+sales['Revenue'] = sales['Revenue'].astype('int')
+
+# Verify type of 'Revenue'
+assert sales['Revenue'].dtype == 'int' # return nothing for True, return error for False
+
+print(sales['Revenue'].sum())
+# %%
+
 # %%
